aad4dab4195ebfd87311bb5349e8b54e9b037694
Amanda Tennent <amandatennent@me.com>
2019-03-06 23:33:18 +1100

    Finsihed video 15 - displaying state with JSX

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,8 +1,10 @@
+/* eslint-disable react/no-unused-state */
 import React from 'react';
 import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
 	state = {
@@ -18,14 +20,19 @@ class App extends React.Component {
 	};
 
 	loadSampleFishes = () => {
-		this.setState({fishes: sampleFishes});
-	}
+		this.setState({ fishes: sampleFishes });
+	};
 
 	render() {
 		return (
 			<div className="catch-of-the-day">
 				<div className="menu">
 					<Header tagline="Fresh Seafood Market" />
+					<ul className="fishes">
+						{Object.keys(this.state.fishes).map(key => (
+							<Fish key={key} details={this.state.fishes[key]}>{key}</Fish>
+						))}
+					</ul>
 				</div>
 				<Order />
 				<Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
