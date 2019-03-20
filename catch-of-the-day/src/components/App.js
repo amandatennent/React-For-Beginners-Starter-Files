d7a659db17be0d908f05e94ab38420398c211d5c
Amanda Tennent <amanda.tennent@inlogik.com>
2019-03-20 13:02:57 +1100

    Finished Video 20 - Bi-directional Data Flow and Live State Editing

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -40,16 +40,23 @@ class App extends React.Component {
 		this.setState({ fishes }); // Set the new fishes object to state
 	};
 
+	updateFish = (key, updatedFish) => {
+		const fishesObject = this.state.fishes; // Take a cope of the existing state
+		const fishes = { ...fishesObject };
+		fishes[key] = updatedFish; // Update the state
+		this.setState({ fishes }); // Set the new state
+	};
+
 	loadSampleFishes = () => {
 		this.setState({ fishes: sampleFishes });
 	};
 
-	addToOrder = (key) => {
+	addToOrder = key => {
 		const orderObject = this.state.order; // Take a copy of state
 		const order = { ...orderObject };
 		order[key] = order[key] + 1 || 1; // Either add to the order, or update the number in the order
 		this.setState({ order }); // Call set state to update state object
-	}
+	};
 
 	render() {
 		return (
@@ -58,17 +65,12 @@ class App extends React.Component {
 					<Header tagline="Fresh Seafood Market" />
 					<ul className="fishes">
 						{Object.keys(this.state.fishes).map(key => (
-							<Fish
-								key={key}
-								index={key}
-								details={this.state.fishes[key]}
-								addToOrder={this.addToOrder}
-							/>
+							<Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />
 						))}
 					</ul>
 				</div>
 				<Order fishes={this.state.fishes} order={this.state.order} />
-				<Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
+				<Inventory addFish={this.addFish} updateFish={this.updateFish} loadSampleFishes={this.loadSampleFishes} fishes={this.state.fishes} />
 			</div>
 		);
 	}
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AddFishForm from './AddFishForm';
+import EditFishForm from './EditFishForm';
 
 /* eslint-disable react/prefer-stateless-function */
 class Inventory extends React.Component {
@@ -7,8 +8,13 @@ class Inventory extends React.Component {
 		return (
 			<div className="inventory">
 				<h2>Inventory</h2>
+				{Object.keys(this.props.fishes).map(key => (
+					<EditFishForm key={key} index={key} fish={this.props.fishes[key]} updateFish={this.props.updateFish} />
+				))}
 				<AddFishForm addFish={this.props.addFish} />
-				<button type="submit" onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
+				<button type="submit" onClick={this.props.loadSampleFishes}>
+					Load Sample Fishes
+				</button>
 			</div>
 		);
 	}
